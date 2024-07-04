51d6a12446b16021c73513af793db3a573035be8
Arthurecomp <arthurecomp22@gmail.com>
2024-07-04 18:32:44 -0300

    ajustes do navbar

--- front-end/src/components/Banner.jsx
+++ front-end/src/components/Banner.jsx
@@ -1,15 +1,23 @@
-import Image from 'next/image';
-import Logo from '@/images/Logo-upe-site.png';
+import Image from "next/image";
+import Logo from "@/images/Logo-upe-site.png";
+import DensityMediumOutlinedIcon from "@mui/icons-material/DensityMediumOutlined";
 
-export default function Banner() {
+export default function Banner({ onclick }) {
   return (
-    <div className="bg-white w-full h-[90px] flex items-center justify-between px-4 shadow-bottom">
-      <Image src={Logo} alt="Logo-UPE" width={100} height={50} priority={true} />
-      <div className="flex items-center">
-        Saúde dos servidores UPE
-      </div>
-
-      
+    <div className="bg-white w-full h-[90px] flex items-center justify-between   px-4 shadow-bottom">
+      <section className=" flex flex-row items-center gap-2">
+        <button onClick={onclick}>
+          <DensityMediumOutlinedIcon />
+        </button>
+        <Image
+          src={Logo}
+          alt="Logo-UPE"
+          width={100}
+          height={50}
+          priority={true}
+        />
+      </section>
+      <div className="flex items-center">Saúde dos servidores UPE</div>
     </div>
   );
 }
--- front-end/src/components/ButtonNav.jsx
+++ front-end/src/components/ButtonNav.jsx
@@ -1,8 +1,9 @@
-import TimelineRoundedIcon from "@mui/icons-material/TimelineRounded";
+import React from "react";
+
 export default function ButtonNav({ title, children }) {
   return (
-    <button className=" flex items-center">
-      <div className=" mr-2">{title}</div>
+    <button className="flex items-center w-full py-6 h-8 transition-transform duration-300 hover:scale-105 hover:bg-gray-200">
+      <div className=" px-2">{title}</div>
       <div>{children}</div>
     </button>
   );
