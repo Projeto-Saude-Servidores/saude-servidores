4a9f2b0c2f3f8ca77f288d3795a038a959093658
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2024-07-08 19:44:10 -0300

    bug corrigido

--- front-end/src/components/GraficoColunas.jsx
+++ front-end/src/components/GraficoColunas.jsx
@@ -1,14 +1,9 @@
 import * as React from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
-<<<<<<< HEAD
-=======
-import { axisClasses } from '@mui/x-charts/ChartsAxis';
->>>>>>> b412d402f154ce2331174637eaceda00ab18ac2b
 import Stack from '@mui/material/Stack';
 import PropTypes from 'prop-types';
 
 function GraficoColunas({ data }) {
-<<<<<<< HEAD
   const getColor = (value) => {
     if (value < 1) {
       return '#76B7B2';
@@ -32,45 +27,13 @@ function GraficoColunas({ data }) {
     return acc;
   }, {});
 
-  const legendItems = [
-    { label: 'bom', color: '#76B7B2' },
-    { label: 'médio', color: '#EDC949' },
-    { label: 'ruim', color: '#E15759' },
-  ];
-
   console.log('dataset:', JSON.stringify(dataset, null, 2));
   console.log('colorMap:', JSON.stringify(colorMap, null, 2));
-=======
-  console.log('Dataset:', data); // Verifica os dados recebidos antes do processamento
-
-
-  // ainda sem funcionar as cores
-  const getColor = (value) => {
-    if (value < 1) {
-      return '#76B7B2';
-    } else if (value >= 1 && value < 2) {
-      return '#EDC949';
-    } else {
-      return '#E15759';
-    }
-  };
-
-  const dataset = React.useMemo(() => {
-    return Object.entries(data).map(([key, value]) => ({
-      order: key,
-      high: value,
-      color: getColor(value), // Adiciona a cor com base nos critérios definidos
-    }));
-  }, [data]);
-
-  console.log('Dataset com cores:', dataset); // Verifica o dataset com as cores atribuídas
->>>>>>> b412d402f154ce2331174637eaceda00ab18ac2b
 
   return (
     <Stack direction="column" spacing={1} sx={{ width: '100%', height: '100%' }}>
       <BarChart
         series={[
-<<<<<<< HEAD
           {
             dataKey: 'high',
             layout: 'vertical',
@@ -89,11 +52,6 @@ function GraficoColunas({ data }) {
           },
         ]}
         yAxis={[]}
-=======
-          { dataKey: 'high', layout, stack: 'stack', colorKey: 'color' },
-        ]}
-        {...(layout === 'vertical' ? chartSettingsV(dataset) : chartSettingsH(dataset))}
->>>>>>> b412d402f154ce2331174637eaceda00ab18ac2b
         borderRadius={10}
         sx={{ height: '80%' }}
         margin={{
@@ -102,17 +60,6 @@ function GraficoColunas({ data }) {
           top: 30,
           bottom: 30,
         }}
-<<<<<<< HEAD
-        slotProps={{
-          legend: {
-            items: legendItems.map((item) => ({
-              label: item.label,
-              marker: { fill: item.color },
-            })),
-          },
-        }}
-=======
->>>>>>> b412d402f154ce2331174637eaceda00ab18ac2b
       />
     </Stack>
   );
@@ -122,23 +69,4 @@ GraficoColunas.propTypes = {
   data: PropTypes.object.isRequired,
 };
 
-<<<<<<< HEAD
-=======
-const chartSettingsH = (dataset) => ({
-  dataset,
-  yAxis: [{ scaleType: 'band', dataKey: 'order' }],
-  sx: {
-    [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
-      transform: 'translateX(-10px)',
-    },
-  },
-});
-
-const chartSettingsV = (dataset) => ({
-  ...chartSettingsH(dataset),
-  xAxis: [{ scaleType: 'band', dataKey: 'order' }],
-  yAxis: undefined,
-});
-
->>>>>>> b412d402f154ce2331174637eaceda00ab18ac2b
 export default GraficoColunas;
