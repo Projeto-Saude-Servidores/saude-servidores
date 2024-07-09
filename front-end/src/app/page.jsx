03161e11d10f0fcf7f5309bd226ef5ef626cc500
Lucas César <112576328+Lucc4s228@users.noreply.github.com>
2024-07-08 21:44:08 -0300

    bloco do gráfico de cima pronto

--- front-end/src/app/page.jsx
+++ front-end/src/app/page.jsx
@@ -1,4 +1,3 @@
-// Importações necessárias
 "use client";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -6,16 +5,14 @@ import GraficoColunas from "@/components/GraficoColunas";
 import NavBar from "@/components/NavBar";
 import SectorPainChart from "@/components/Sector";
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
-import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+// import AssignmentIndIcon from '@mui/icons-material/AssignmentInd'; 
 import OverviewData from "@/components/Overview";
 
-// Componente Home principal
 export default function Home() {
   const [responseData, setResponseData] = useState({});
   const [selectedSector, setSelectedSector] = useState(null);
   const [sectorData, setSectorData] = useState({});
 
-  // Hook useEffect para carregar dados iniciais
   useEffect(() => {
     axios
       .get("http://127.0.0.1:5000/api/setores")
@@ -48,20 +45,21 @@ export default function Home() {
 
   return (
     <NavBar>
-      <div className="h-screen flex flex-row flex-grow gap-5 p-7 w-full justify-center items-center bg-gray-200">
+      <div className="h-screen flex flex-row flex-grow gap-5 p-5 w-full items-center bg-gray-200">
         <div className="flex flex-col gap-5 w-1/2 justify-center items-center h-full">
-          <div className="bg-white rounded-lg h-1/2 w-full justify-center pt-3 shadow-md flex flex-col items-center">
-            <div className="bg-white rounded-full border-black border-2 pt-1 pb-1 px-2 text-center flex items-center justify-center w-auto">
-              <AssignmentIndIcon /> Nível de dores por departamento
+          <div className="bg-white rounded-lg h-1/2 w-full pt-3 shadow-md flex flex-col items-center">
+            <div className="w-full text-left pl-4 pb-1 font-bold">
+              Nível de dores por departamento
+              {/* tirei o icon por enquanto, mas pode colocar de volta se achar mais bonito */}
             </div>
-            <div className="rounded-md w-full h-full">
+            <div className="rounded-md justify-center w-full h-full">
               <GraficoColunas data={responseData} />
             </div>
           </div>
 
-          <div className="bg-white rounded-lg h-1/2 w-full shadow-md  flex flex-col items-center">
+          <div className="bg-white rounded-lg h-1/2 w-full shadow-md flex flex-col items-center">
             <div className="pb-3"></div>
-            <div className="bg-white rounded-full border-black border-2  pt-1 pb-1 text-center flex items-center justify-center w-48">
+            <div className="bg-white rounded-full border-black border-2 pt-1 pb-1 text-center flex items-center justify-center w-48">
               <ManageSearchIcon /> Selecione um Setor
             </div>
             <div className="p-2"></div>
@@ -83,7 +81,7 @@ export default function Home() {
 
         <div className="flex flex-col h-full w-1/2 justify-start items-center">
           <div className="bg-white rounded-lg h-full w-full pt-3 shadow-md flex flex-col justify-start items-center">
-            <div className="bg-white rounded-full border-black border-2 pl-3 pr-3 pt-1 pb-1 text-center">
+            <div className="bg-white rounded-full mb-1 border-black border-2 pl-3 pr-3 pt-1 pb-1 text-center">
               Resumo das Respostas
             </div>
             {selectedSector && <OverviewData sectorData={sectorData} />}
@@ -93,5 +91,3 @@ export default function Home() {
     </NavBar>
   );
 }
-
-//
